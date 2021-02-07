86501036ffa1c5b778eaf07e66db217b5cfc2490
심종훈 <sayhello.shim@gmail.com>
2021-02-07 15:26:24 +0900

    origin dev 백업용입니다

--- ssangdaewithyou-client/src/components/List.js
+++ ssangdaewithyou-client/src/components/List.js
@@ -9,6 +9,20 @@ function List({ cartItems }) {
     <div id="list">
       List
       <div id="enroll">Enroll</div>
+      {/* <div id="listEl">
+        <div className="listPlaceName">장소명</div>
+        <div className="listPlaceUser">정복자</div>
+        <div className="countLikeOrDislike">
+          <div className="listLike">
+            <img src="../img/like.jpg" />
+            <div className="count">7</div>
+          </div>
+          <div className="dislistLike">
+            <img src="../img/dislike.jpg" />
+            <div className="count">2</div>
+          </div>
+        </div>
+      </div> */}
     </div>
   );
 }
--- ssangdaewithyou-client/src/components/Map.js
+++ ssangdaewithyou-client/src/components/Map.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { RenderAfterNavermapsLoaded, NaverMap } from 'react-naver-maps'
+
 // !
 import { Link } from 'react-router-dom';
 
@@ -7,7 +9,19 @@ function Map({ cartItems }) {
 
   return (
     <div id="map">
-      Map
+      <RenderAfterNavermapsLoaded
+        ncpClientId={process.env.REACT_APP_NAVER_MAP_API_KEY}
+        error={<p>Maps Load Error</p>}
+        loading={<p>Maps Loading...</p>}
+      >
+        <NaverMap 
+          id='maps-examples-map-simple'
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+        />
+      </RenderAfterNavermapsLoaded>
     </div>
   );
 }
