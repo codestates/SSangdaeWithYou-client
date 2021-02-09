746bc05ef7ff07037773447fce623175e1838f4a
심종훈 <sayhello.shim@gmail.com>
2021-02-09 18:48:35 +0900

    [CLIENT] [MERGER] merge를 위한 commit

--- ssangdaewithyou-client/src/fakeData/fakeData.js
+++ ssangdaewithyou-client/src/fakeData/fakeData.js
@@ -7,7 +7,8 @@ export const fakeData =
       "latitude": 37.554722,
       "longitude": 126.970833,
       "comment": "좋군",
-      "userId": "김철수",
+      "userId": 1,
+      "nickname": "김철수",
     },
     {
       "id": 2,
@@ -15,7 +16,8 @@ export const fakeData =
       "latitude": 37.4923164,
       "longitude": 126.9182618,
       "comment": "괜찮아",
-      "userId": "이영희",
+      "userId": 2,
+      "nickname": "이영희",
     },
     {
       "id": 3,
@@ -23,17 +25,61 @@ export const fakeData =
       "latitude": 37.503382,
       "longitude": 127.0184262,
       "comment": "굉장해",
-      "userId": "정민수",
+      "userId": 3,
+      "nickname": "정민수",
     },
   ],
-  "cartItems": [
+  "messages": [
     {
-      "itemId": 1,
-      "quantity": 1
+      "id": 1,
+      "placeId": 1,
+      "userId": 1,
+      "message": 1,
     },
     {
-      "itemId": 5,
-      "quantity": 7
+      "id": 1,
+      "placeId": 1,
+      "userId": 1,
+      "message": 1,
     } 
+  ],
+  "likeOrDislike": [
+    {
+      "id": 1,
+      "userId": 1,
+      "placeId": 1,
+      "isLike": 1,
+      "isDislike": 1,
+    },
+    {
+      "id": 1,
+      "userId": 1,
+      "placeId": 1,
+      "isLike": 1,
+      "isDislike": 1,
+    },
+  ],
+  "users": [
+    {
+      "id": 1,
+      "username": "kimtheking",
+      "password": "asdf",
+      "nickname": "김철수",
+      "email": "kim@asdf.com"
+    },
+    {
+      "id": 2,
+      "username": "leevis",
+      "password": "asdf",
+      "nickname": "이영희",
+      "email": "lee@asdf.com"
+    },
+    {
+      "id": 3,
+      "username": "orionchocopie",
+      "password": "asdf",
+      "nickname": "이영희",
+      "email": "zung@asdf.com"
+    },
   ]
 }
