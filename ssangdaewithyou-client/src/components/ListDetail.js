16fc2f3dbde428d1ce72ee2e7f5ab22a1a166db1
in63119 <in63119@gmail.com>
2021-02-09 18:41:03 +0900

    [CLIENT] [FIX] conflict 해결

--- ssangdaewithyou-client/src/components/ListDetail.js
+++ ssangdaewithyou-client/src/components/ListDetail.js
@@ -19,65 +19,64 @@ function ListDetail({ existingPlaceInfo }) {
             <div id="placeEl">{existingPlaceInfo.comment}</div>
           </div>
         </div>
-        
-          <div id="likeOrDislike">
-            <div className="forLikeOrDislikeFrame">
-              <div className="count">7</div>
-              <div id="like">
-                <img src="../img/like.jpg" className="likeOrDislikeImg" alt="profile"/>
-                <div id="likeOrDislikeText">추천해요</div>
-              </div>
-            </div>
-            <div className="forLikeOrDislikeFrame">
-              <div className="count">7</div>
-              <div id="disLike">
-                <img src="../img/dislike.jpg" className="likeOrDislikeImg" alt="profile"/>
-                <div id="likeOrDislikeText">비추해요</div>
-              </div>
+        <div id="likeOrDislike">
+          <div className="forLikeOrDislikeFrame">
+            <div className="count">7</div>
+            <div id="like">
+              <img src="../img/like.jpg" className="likeOrDislikeImg" alt="profile"/>
+              <div id="likeOrDislikeText">추천해요</div>
             </div>
           </div>
-          <div id="message">
-            <div id="userMessage">
-              <div id="userId">ID</div>
-              <textarea placeholder="댓글 추가" id="userText"></textarea>
-              <div className="forUserMessageTop"></div>
-              <div id="forAlignButton">
-                <div id="messageButton">
-                  <div id="messageButtonText">submit</div>
-                </div>
-              </div>
-            </div>
-            <div id="questList">
-              <div id="idOrDate">
-                <div className="guestId">대장아재</div>
-                <div className="guestDate">2021-02-04 21:50:20</div>
-              </div>
-              <div className="guestText">꺄르르르르르르 우리 아재들 담배 맛이쪄요?</div>
-            </div>
-            <div id="questList">
-              <div id="idOrDate">
-                <div className="guestId">새끼아재</div>
-                <div className="guestDate">2021-02-04 21:50:20</div>
-              </div>
-              <div className="guestText">끼릭끼릭 츄릅츄릅 햘딱햘딱</div>
-            </div>
-            <div id="questList">
-              <div id="idOrDate">
-                <div className="guestId">둘째아재</div>
-                <div className="guestDate">2021-02-04 21:50:20</div>
-              </div>
-              <div className="guestText">저는 담배를 안피워요. 저는 두줄 이상을 써야하는 임무가 있답니다. 아핳아핳 호로로로로롤로롤</div>
-            </div>
-            <div id="questList">
-              <div id="idOrDate">
-                <div className="guestId">중간아재</div>
-                <div className="guestDate">2021-02-04 21:50:20</div>
-              </div>
-              <div className="guestText">나 연초피는줄 알아찌?! 데헷데헷 나는 전자담배 핀다. 데헷데헷</div>
+          <div className="forLikeOrDislikeFrame">
+            <div className="count">7</div>
+            <div id="disLike">
+              <img src="../img/dislike.jpg" className="likeOrDislikeImg" alt="profile"/>
+              <div id="likeOrDislikeText">비추해요</div>
             </div>
           </div>
         </div>
+        <div id="message">
+          <div id="userMessage">
+            <div id="userId">ID</div>
+            <textarea placeholder="댓글 추가" id="userText"></textarea>
+            <div className="forUserMessageTop"></div>
+            <div id="forAlignButton">
+              <div id="messageButton">
+                <div id="messageButtonText">submit</div>
+              </div>
+            </div>
+          </div>
+          <div id="questList">
+            <div id="idOrDate">
+              <div className="guestId">대장아재</div>
+              <div className="guestDate">2021-02-04 21:50:20</div>
+            </div>
+            <div className="guestText">꺄르르르르르르 우리 아재들 담배 맛이쪄요?</div>
+          </div>
+          <div id="questList">
+            <div id="idOrDate">
+              <div className="guestId">새끼아재</div>
+              <div className="guestDate">2021-02-04 21:50:20</div>
+            </div>
+            <div className="guestText">끼릭끼릭 츄릅츄릅 햘딱햘딱</div>
+          </div>
+          <div id="questList">
+            <div id="idOrDate">
+              <div className="guestId">둘째아재</div>
+              <div className="guestDate">2021-02-04 21:50:20</div>
+            </div>
+            <div className="guestText">저는 담배를 안피워요. 저는 두줄 이상을 써야하는 임무가 있답니다. 아핳아핳 호로로로로롤로롤</div>
+          </div>
+          <div id="questList">
+            <div id="idOrDate">
+              <div className="guestId">중간아재</div>
+              <div className="guestDate">2021-02-04 21:50:20</div>
+            </div>
+            <div className="guestText">나 연초피는줄 알아찌?! 데헷데헷 나는 전자담배 핀다. 데헷데헷</div>
+          </div>
+        </div>
       </div>
+    </div>
   );
 }
 
