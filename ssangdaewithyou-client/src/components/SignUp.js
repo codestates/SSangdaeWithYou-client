a425bba5e1f0c9ac9a29a2accf0af4ff5278493f
in63119 <in63119@gmail.com>
2021-02-10 00:49:37 +0900

    [CLIENT] [FIX] 유효성 검사 test

--- ssangdaewithyou-client/src/components/SignUp.js
+++ ssangdaewithyou-client/src/components/SignUp.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useState } from 'react';
 
 function SignUp() {
+  // 상태관리로 기본 true를 주고 
+  // 어떠한 조건이 부합하면 true, 부합하지 않으면 false
+  // false가 되면 display: none; 이였던 유효성 검사를 block으로.
+
   return (
     <div id="modalFrame">
       <div className="modalLogo forSignupLogo">
@@ -8,8 +13,10 @@ function SignUp() {
         <div className="modalLogoText1">쌍대는 너랑</div>
         <div className="modalLogoText2">(돗대는 나만)</div>
       </div>
-      <input type="text" placeholder="아이디" className="SignUp"/>  
-      <input type="text" placeholder="비밀번호" className="SignUp"/>
+      <input type="text" placeholder="아이디" className="SignUp"/>
+      {/* <div className="dfdf">아이디가 정확하지 않습니다.</div>   */}
+      <input type="password" placeholder="비밀번호" className="SignUp"/>
+      <input type="password" placeholder="비밀번호 확인" className="SignUp"/>
       <input type="text" placeholder="e-mail" className="SignUp"/>
       <input type="text" placeholder="닉네임" className="SignUp"/>
       <div className="SignUpButton">
