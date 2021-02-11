69e73300adfe0df762d09e7e152766e8dde2fe67
in63119 <in63119@gmail.com>
2021-02-12 01:54:18 +0900

    [CLIENT] [REMOVE] 날짜&시간

--- ssangdaewithyou-client/src/components/Message.js
+++ ssangdaewithyou-client/src/components/Message.js
@@ -6,7 +6,6 @@ function Message({ message }) {
     <div id="questList">
       <div id="idOrDate">
         <div className="guestId">{message.nickname}</div>
-        <div className="guestDate">2021-02-04 21:50:20</div>
       </div>
       <div className="guestText">{message.message}</div>
     </div>  
